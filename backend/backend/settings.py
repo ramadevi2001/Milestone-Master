6236dac708bc286446d73369b847f4329e2a4e51
ramadevi2001 <ramadevicheruku49@gmail.com>
2024-06-19 06:43:50 +0530

    put installed apps in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "ENGINE": "django.db.backends.postgresql",
+        "NAME": "restapidb",
+        "USER": "postgres",
+        "PASSWORD": "ramadevi",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
@@ -121,3 +125,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# myproject/settings.py
+
+INSTALLED_APPS = [
+'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'myapp',
+]
